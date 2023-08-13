1ce758b07ad935d07d82de268ef6beb0b6e71ee0
Idriss Chefai <namidid2.0@gmail.com>
2023-08-13 12:50:34 +0100

    Added Navbar components

---
+++ blog/src/Navbar.js
@@ -0,0 +1,16 @@
+
+
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Draysis Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create a new Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
