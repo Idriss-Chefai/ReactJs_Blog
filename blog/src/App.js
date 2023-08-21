3a7bb0b854ded06fd715901d2775c3ce88036f11
Idriss Chefai <namidid2.0@gmail.com>
2023-08-21 13:57:50 +0100

    Added few components and custom hook and added router and links

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router ,Route , Switch} from "react-router-dom";
+import Create from './create';
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
       <Navbar></Navbar>
       <div className="content">
-        <Home/>
+        <Switch>
+          <Route exact path = "/"> <Home/> </Route>
+          <Route exact path = "/create"> <Create/> </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
